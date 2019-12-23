7075ed1cdd97c90a6cb95b8ef6db526af05becaa
David Simões <2170978@my.ipleiria.pt>
2019-12-23 18:46:26 +0000

    Some DTOs and Controllers updated

--- src/ws/HourTimeController.java
+++ src/ws/HourTimeController.java
@@ -1,4 +1,87 @@
 package ws;
 
+import dtos.HourTimeDTO;
+import ejbs.HourTimeBean;
+import entities.HourTime;
+
+import javax.ejb.EJB;
+import javax.ejb.EJBException;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Path("/hourTime")
+@Produces({MediaType.APPLICATION_JSON})
+@Consumes({MediaType.APPLICATION_JSON})
 public class HourTimeController {
+    @EJB
+    private HourTimeBean hourTimeBean;
+    public static HourTimeDTO toDTO(HourTime hourTime) {
+        return new HourTimeDTO(
+                hourTime.getId(),
+                hourTime.getHour(),
+                hourTime.getMinutes()
+        );
+    }
+
+    public static List<HourTimeDTO> toDTOs(Collection<HourTime> hourTimes) {
+        return hourTimes.stream().map(HourTimeController::toDTO).collect(Collectors.toList());
+    }
+
+    @GET
+    @Path("/")
+    public List<HourTimeDTO> all() {
+        try {
+            return toDTOs(hourTimeBean.all());
+        } catch (Exception e) {
+            throw new EJBException("ERROR_GET_COACHES", e);
+        }
+    }
+
+    @GET
+    @Path("{id}")
+    public Response getAdministratorDetails(@PathParam("id") int id) throws Exception {
+        HourTime hourTime = hourTimeBean.find(id);
+        try{
+            return Response.status(Response.Status.OK).entity(toDTO(hourTime)).build();
+        } catch (Exception e) {
+            throw new EJBException("ERROR_GET_ATHLETES", e);
+        }
+    }
+
+    @POST
+    @Path("/")
+    public Response createNewAthlete (HourTimeDTO hourTimeDTO) throws Exception {
+        hourTimeBean.create(hourTimeDTO.getHour(),hourTimeDTO.getMinutes());
+        try{
+            return Response.status(Response.Status.CREATED).build();
+        } catch (Exception e) {
+            throw new EJBException("ERROR_CREATING_ATHLETE", e);
+        }
+    }
+
+    /*@PUT
+    @Path("/{username}")
+    public Response updateAthlete (HourTimeDTO hourTimeDTO) throws Exception {
+        HourTime hourTime = hourTimeBean.update(athleteDTO.getUsername(),athleteDTO.getPassword(),athleteDTO.getName(),athleteDTO.getEmail());
+        try{
+            return Response.status(Response.Status.CREATED).entity(toDTO(athlete)).build();
+        } catch (Exception e) {
+            throw new EJBException("ERROR_UPDATING_ATHLETE", e);
+        }
+    }*/
+
+    @DELETE
+    @Path("/{id}")
+    public Response deleteAthlete(@PathParam("id") int id) throws Exception {
+        hourTimeBean.delete(id);
+        try{
+            return Response.status(Response.Status.OK).build();
+        } catch (Exception e) {
+            throw new EJBException("ERROR_DELETING_COACH", e);
+        }
+    }
 }
--- src/ws/ScheduleController.java
+++ src/ws/ScheduleController.java
@@ -1,4 +1,77 @@
 package ws;
 
+import dtos.ScheduleDTO;
+import ejbs.ScheduleBean;
+import entities.Schedule;
+
+import javax.ejb.EJB;
+import javax.ejb.EJBException;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Path("/schedules")
+@Produces({MediaType.APPLICATION_JSON})
+@Consumes({MediaType.APPLICATION_JSON})
 public class ScheduleController {
+    @EJB
+    private ScheduleBean scheduleBean;
+    public static ScheduleDTO toDTO(Schedule schedule) {
+        return new ScheduleDTO(
+                schedule.getId(),
+                schedule.getDayOfWeek().getValue(),
+                HourTimeController.toDTO(schedule.getStartDate()),
+                HourTimeController.toDTO(schedule.getEndDate())
+        );
+    }
+
+    public static List<ScheduleDTO> toDTOs(Collection<Schedule> schedules) {
+        return schedules.stream().map(ScheduleController::toDTO).collect(Collectors.toList());
+    }
+
+    @GET
+    @Path("/")
+    public List<ScheduleDTO> all() {
+        try {
+            return toDTOs(scheduleBean.all());
+        } catch (Exception e) {
+            throw new EJBException("ERROR_GET_COACHES", e);
+        }
+    }
+
+    @GET
+    @Path("{id}")
+    public Response getAdministratorDetails(@PathParam("id") int id) throws Exception {
+        Schedule schedule = scheduleBean.find(id);
+        try{
+            return Response.status(Response.Status.OK).entity(toDTO(schedule)).build();
+        } catch (Exception e) {
+            throw new EJBException("ERROR_GET_ATHLETES", e);
+        }
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response updateAthlete (ScheduleDTO scheduleDTO) throws Exception {
+        Schedule schedule = scheduleBean.update(scheduleDTO.getId(),scheduleDTO.getDayOfWeek(),scheduleDTO.getStartDate().getId(),scheduleDTO.getEndDate().getId());
+        try{
+            return Response.status(Response.Status.CREATED).entity(toDTO(schedule)).build();
+        } catch (Exception e) {
+            throw new EJBException("ERROR_UPDATING_ATHLETE", e);
+        }
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response deleteAthlete(@PathParam("id") int id) throws Exception {
+        scheduleBean.delete(id);
+        try{
+            return Response.status(Response.Status.OK).build();
+        } catch (Exception e) {
+            throw new EJBException("ERROR_DELETING_COACH", e);
+        }
+    }
 }
